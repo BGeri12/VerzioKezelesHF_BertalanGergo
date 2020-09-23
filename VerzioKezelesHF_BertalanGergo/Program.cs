93058d13ee42c73f1d6484d6b57126f0e38e0bfc
BGeri12 <gerib2001@gmail.com>
2020-09-23 17:16:18 +0200

    Beolvasok 10 számot a felhasználótól és kiszámolom az átlagukat.

--- VerzioKezelesHF_BertalanGergo/Program.cs
+++ VerzioKezelesHF_BertalanGergo/Program.cs
@@ -6,7 +6,17 @@ namespace VerzioKezelesHF_BertalanGergo
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			Console.WriteLine("Kérem adjon meg tíz számot.");
+
+			int darab = 10;
+			int[] tomb = new int[darab];
+			int osszeg = 0;
+			for (int i = 0; i < darab; i++)
+			{
+				tomb[i] = int.Parse(Console.ReadLine());
+				osszeg = osszeg + tomb[i];
+			}
+			Console.WriteLine("A tíz számnak az átlaga: {0}",osszeg/darab);
 		}
 	}
 }
